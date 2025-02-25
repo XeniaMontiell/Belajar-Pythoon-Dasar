65ac2c739e8c0956ca6fbdbb78c1c7531b7abeb9
XeniaMontiell <xeniamontiell@gmail.com>
2025-02-25 18:50:42 +0700

    Add files via upload

---
+++ Belajar Python Tingkat Dasar.py
@@ -0,0 +1,14 @@
+T01 = Nasi Goreng
+T02 = Mie Goreng
+T03 = Ayam Goreng
+T04 = Ayam Bakar
+T05 = Ayam Sayur
+T06 = Tempe Goreng
+T07 = Tahu Goreng
+T08 = Soto
+T09 = Nasi Uduk
+T10 = Nasi Kuning
+T11 = Nasi Rames
+T12 = Nasi Padang
+
+print ("Menu Makanan")
